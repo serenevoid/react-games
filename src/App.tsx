4a125f57dc3bea65cd4a155e976f8891375f0cab
Serene Void <ajuph9224@gmail.com>
2023-03-21 21:16:11 +0530

    chore: completed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,91 @@
+import React from "react";
 import "./style.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 const App = () => {
+
+    const [dieValues, setDieValues] = React.useState(rollDice());
+    const [tenzies, setTenzies] = React.useState(false);
+
+    React.useEffect(() => {
+        if (dieValues.every(die => die.isHeld)) {
+            const first = dieValues[0].value;
+            const allSameValues = dieValues.every(die => die.value === first);
+            if (allSameValues) {
+                setTenzies(true);
+            }
+        }
+    }, [dieValues]);
+
+    function generateDie() {
+        return {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false
+        };
+    }
+
+    function rollDice() {
+        const newValues = [];
+        for (let i = 0; i < 10; i++) {
+            newValues.push(generateDie());
+        }
+        return newValues;
+    }
+
+    function roll() {
+        if (!tenzies) {
+            setDieValues(currentValues => {
+                return currentValues.map(die => {
+                    if (!die.isHeld) {
+                        return generateDie();
+                    } else
+                        return die;
+                });
+            });
+        }
+    }
+
+    function reset() {
+        setDieValues(rollDice());
+        setTenzies(false);
+    }
+
+    function toggleHold(key: string) {
+        setDieValues(dieValues => dieValues.map(die => {
+            if (die.id === key) {
+                die.isHeld = !die.isHeld;
+            }
+            return die;
+        }));
+    }
+
+    const dice = dieValues.map(die => {
+            return <Die key={die.id} value={die.value.toString()} isHeld={die.isHeld} toggleHold={() => toggleHold(die.id)} />
+        }
+    )
+
     return (
         <div className="container">
+            <h1>Tenzies</h1>
+            <p>
+                Roll until all slots have the same number. Click a slot to freeze it.
+            </p>
             <div className="die_container">
-                <Die value="1"/>
-                <Die value="2"/>
-                <Die value="3"/>
-                <Die value="3"/>
-                <Die value="4"/>
-                <Die value="3"/>
-                <Die value="5"/>
-                <Die value="6"/>
-                <Die value="4"/>
-                <Die value="5"/>
+                {dice}
             </div>
+            <div
+                className={tenzies ? "button new_game" : "button"}
+                onClick={tenzies ? reset : roll}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </div>
+            {tenzies && <Confetti />}
         </div>
     )
+
 }
 
 export default App;
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,10 +1,15 @@
+import { MouseEventHandler } from "react";
+
 interface props {
+    key: string;
     value: string;
+    isHeld: boolean;
+    toggleHold: MouseEventHandler;
 }
 
 const Die = (props: props) => {
     return (
-        <div className="die">
+        <div className={props.isHeld ? "die held" : "die"} onClick={props.toggleHold}>
             <h2>{props.value}</h2>
         </div>
     )
